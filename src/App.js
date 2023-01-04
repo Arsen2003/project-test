952d5215b7622410bf39bf6ed3f56eebe6d4217b
Arsen2003 <arsen_nurdavletov@mail.ru>
2023-01-04 14:21:49 +0600

    add files to githab

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import AuthContextProvider from "./contexts/AuthContextProvider";
+import ProductContextProvider from "./contexts/ProductContextProvider";
+import MainRoutes from "./MainRoutes";
+
+function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <ProductContextProvider>
+          <Navbar />
+          <MainRoutes />
+        </ProductContextProvider>
+      </AuthContextProvider>
+    </>
+  );
+}
+export default App;
